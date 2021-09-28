08bd1bafa1e290883f2c9626efb8596a03744786
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-09-28 04:09:13 +0400

    Added game over test cases when a row is complete.

---
+++ src/main/java/xogame/game_over/GameOver.java
@@ -0,0 +1,25 @@
+package xogame.game_over;
+
+import xogame.models.Board;
+
+public class GameOver {
+    public static Boolean isGameOver(Board board) {
+        return null;
+    }
+
+    public static Boolean isARowComplete(int rowNumber, Board board) {
+        return null;
+    }
+
+    public static Boolean isAColumnComplete(int rowNumber, Board board) {
+        return null;
+    }
+
+    public static Boolean isADiagonalComplete(Board board) {
+        return null;
+    }
+
+    public static Boolean isTheBoardFull(Board board) {
+        return null;
+    }
+}
