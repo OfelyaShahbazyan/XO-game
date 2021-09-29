66e98c1871d25ce7bc37997da1782bd0bb5b262a
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-09-29 15:31:18 +0400

    Added game over(by columns) test cases.

--- src/test/java/xogame/game_over/GameOverTest.java
+++ src/test/java/xogame/game_over/GameOverTest.java
@@ -75,4 +75,58 @@ public class GameOverTest {
         Board board = new Board(new int[] { 2, 2, 0, 0, 0, 0, 0, 0, 0 });
         assertFalse(GameOver.isAnyRowComplete(board));
     }
+
+    @Test
+    public void the_first_column_is_complete_if_it_contains_all_ones() {
+        Board board = new Board(new int[] { 1, 2, 0, 1, 2, 0, 1, 0, 0 });
+        assertTrue(GameOver.isAnyColumnComplete(board));
+    }
+
+    @Test
+    public void the_first_column_is_complete_if_it_contains_all_twos() {
+        Board board = new Board(new int[] { 2, 1, 0, 2, 1, 0, 2, 0, 0 });
+        assertTrue(GameOver.isAnyColumnComplete(board));
+    }
+
+    @Test
+    public void the_first_column_is_complete_if_it_contains_all_zeros() {
+        Board board = new Board(new int[] { 0, 0, 0, 0, 1, 2, 0, 1, 0 });
+        assertFalse(GameOver.isAnyColumnComplete(board));
+    }
+
+    @Test
+    public void the_second_column_is_complete_if_it_contains_all_ones() {
+        Board board = new Board(new int[] { 2, 1, 0, 2, 1, 0, 0, 1, 0 });
+        assertTrue(GameOver.isAnyColumnComplete(board));
+    }
+
+    @Test
+    public void the_second_column_is_complete_if_it_contains_all_twos() {
+        Board board = new Board(new int[] { 1, 2, 0, 1, 2, 1, 0, 2, 0 });
+        assertTrue(GameOver.isAnyColumnComplete(board));
+    }
+
+    @Test
+    public void the_second_column_is_complete_if_it_contains_all_zeros() {
+        Board board = new Board(new int[] { 0, 0, 0, 0, 0, 2, 1, 0, 1 });
+        assertFalse(GameOver.isAnyColumnComplete(board));
+    }
+
+    @Test
+    public void the_third_column_is_complete_if_it_contains_all_ones() {
+        Board board = new Board(new int[] { 2, 1, 1, 2, 1, 1, 0, 0, 1 });
+        assertTrue(GameOver.isAnyColumnComplete(board));
+    }
+
+    @Test
+    public void the_third_column_is_complete_if_it_contains_all_twos() {
+        Board board = new Board(new int[] { 1, 0, 2, 1, 0, 2, 0, 1, 2 });
+        assertTrue(GameOver.isAnyColumnComplete(board));
+    }
+
+    @Test
+    public void the_third_column_is_complete_if_it_contains_all_zeros() {
+        Board board = new Board(new int[] { 0, 0, 0, 0, 0, 0, 1, 0, 0 });
+        assertFalse(GameOver.isAnyColumnComplete(board));
+    }
 }
