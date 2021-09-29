98e9c89ac2eb9182f650155c7afdfa6067035ecb
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-09-29 17:31:20 +0400

    Added method which checks if any column is complete.

--- src/main/java/xogame/game_over/GameOver.java
+++ src/main/java/xogame/game_over/GameOver.java
@@ -1,4 +1,25 @@
 package xogame.game_over;
 
 public class GameOver {
+    public static Boolean isGameOver(Board board) {
+        return null;
+    }
+
+    public static Boolean isAnyColumnComplete(Board board) {
+        for (int i = 0; i < board.getColumns().size(); i++) {
+            Predicate<Integer> streamsPredicate = item -> item == 1 || item == 2;
+            if (board.getColumns().get(i).stream().allMatch(streamsPredicate)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Boolean isADiagonalComplete(Board board) {
+        return null;
+    }
+
+    public static Boolean isTheBoardFull(Board board) {
+        return null;
+    }
 }
