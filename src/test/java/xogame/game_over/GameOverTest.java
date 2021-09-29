6d49e2ef9d275c17066e375f7b52d9b121c7803b
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-09-29 17:42:40 +0400

    Added method which checks if game over(by columns) and it's test cases.

--- src/test/java/xogame/game_over/GameOverTest.java
+++ src/test/java/xogame/game_over/GameOverTest.java
@@ -9,6 +9,12 @@ import org.junit.jupiter.api.Test;
 import xogame.models.Board;
 
 public class GameOverTest {
+    
+    @Test
+    public void get_columns_of_the_board() {
+        Board board = new Board(new int[] { 2, 2, 0, 1, 1, 1, 0, 0, 0 });
+        assertEquals(3, board.getColumns().size());
+    }
 
     @Test
     public void the_first_column_is_complete_if_it_contains_all_ones() {
