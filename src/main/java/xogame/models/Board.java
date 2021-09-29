9d58aed43b5b2c2eb602c9c5d167d60f3648d131
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-09-29 17:38:25 +0400

    Added method which gets all columns of the board.

--- src/main/java/xogame/models/Board.java
+++ src/main/java/xogame/models/Board.java
@@ -1,6 +1,8 @@
 package xogame.models;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Board {
     private final int[] board;
@@ -15,5 +17,17 @@ public class Board {
 
     public int[] getBoard() {
         return Arrays.copyOf(board, board.length);
+    }
+
+    public List<List<Integer>> getColumns() {
+        List<List<Integer>> columns = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            columns.add(Arrays.asList(board[0 + i], board[3 + i], board[6 + i]));
+        }
+        return columns;
+    }
+
+    public List<List<Integer>> getDiagonals() {
+        return null;
     }
 }
