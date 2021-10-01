24a763a807e5a0a5935218579e1d7b93151f9f0f
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-10-01 12:38:35 +0400

    Added method which checks game is over if any column is complete.

--- src/main/java/xogame/game_over/GameOver.java
+++ src/main/java/xogame/game_over/GameOver.java
@@ -5,29 +5,15 @@ import java.util.function.Predicate;
 import xogame.models.Board;
 
 public class GameOver {
-    public static Boolean isGameOver(Board board) {
-        return null;
-    }
-
-    public static Boolean isAnyRowComplete(Board board) {
-        return null;
-    }
 
     public static Boolean isAnyColumnComplete(Board board) {
+        Predicate<Integer> streamsPredicate = item -> item == 1 || item == 2;
+
+        boolean accumulator = false;
+
         for (int i = 0; i < board.getColumns().size(); i++) {
-            Predicate<Integer> streamsPredicate = item -> item == 1 || item == 2;
-            if (board.getColumns().get(i).stream().allMatch(streamsPredicate)) {
-                return true;
-            }
+            accumulator |= board.getColumns().get(i).stream().allMatch(streamsPredicate);
         }
-        return false;
-    }
-
-    public static Boolean isADiagonalComplete(Board board) {
-        return null;
-    }
-
-    public static Boolean isTheBoardFull(Board board) {
-        return null;
+        return accumulator;
     }
 }
--- src/test/java/xogame/game_over/GameOverTest.java
+++ src/test/java/xogame/game_over/GameOverTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
 
 import xogame.models.Board;
@@ -17,6 +20,14 @@ public class GameOverTest {
     }
 
     @Test
+    public void check_content_of_the_board() {
+        Board board = new Board(new int[] { 2, 1, 0, 0, 2, 1, 1, 0, 2 });
+        assertEquals(Arrays.stream(new int[] { 2, 0, 1 }).boxed().collect(Collectors.toList()), board.getColumns().get(0));
+        assertEquals(Arrays.stream(new int[] { 1, 2, 0 }).boxed().collect(Collectors.toList()), board.getColumns().get(1));
+        assertEquals(Arrays.stream(new int[] { 0, 1, 2 }).boxed().collect(Collectors.toList()), board.getColumns().get(2));
+    }
+    
+    @Test
     public void the_first_column_is_complete_if_it_contains_all_ones() {
         Board board = new Board(new int[] { 1, 2, 0, 1, 2, 0, 1, 0, 0 });
         assertTrue(GameOver.isAnyColumnComplete(board));
@@ -29,7 +40,7 @@ public class GameOverTest {
     }
 
     @Test
-    public void the_first_column_is_complete_if_it_contains_all_zeros() {
+    public void the_first_column_is_not_complete_if_it_contains_all_zeros() {
         Board board = new Board(new int[] { 0, 0, 0, 0, 1, 2, 0, 1, 0 });
         assertFalse(GameOver.isAnyColumnComplete(board));
     }
@@ -47,7 +58,7 @@ public class GameOverTest {
     }
 
     @Test
-    public void the_second_column_is_complete_if_it_contains_all_zeros() {
+    public void the_second_column_is_not_complete_if_it_contains_all_zeros() {
         Board board = new Board(new int[] { 0, 0, 0, 0, 0, 2, 1, 0, 1 });
         assertFalse(GameOver.isAnyColumnComplete(board));
     }
@@ -65,7 +76,7 @@ public class GameOverTest {
     }
 
     @Test
-    public void the_third_column_is_complete_if_it_contains_all_zeros() {
+    public void the_third_column_is_not_complete_if_it_contains_all_zeros() {
         Board board = new Board(new int[] { 0, 0, 0, 0, 0, 0, 1, 0, 0 });
         assertFalse(GameOver.isAnyColumnComplete(board));
     }
