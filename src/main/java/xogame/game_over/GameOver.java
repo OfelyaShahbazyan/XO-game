c22e308ab2cd3ace77d5432ba52bbc080f1a48b1
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-10-03 22:24:06 +0400

    [TEST] Added tests to check is list satisfy to the condition.

--- src/main/java/xogame/game_over/GameOver.java
+++ src/main/java/xogame/game_over/GameOver.java
@@ -1,6 +1,13 @@
 package xogame.game_over;
 
+import java.util.List;
+import java.util.function.Predicate;
+
 public class GameOver {
+    public static Boolean isListSatisfyToTheCondition(List<List<Integer>> list, Predicate<Integer> condition) {
+        return null;
+    }
+
     public static Boolean isBitwiseOrAssignmentOperator(Boolean firstOperand, Boolean secondOperand) {
         return firstOperand |= secondOperand;
     }
