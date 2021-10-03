da7d27ba24553e732301af3957371ef6855c38fe
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-10-03 15:27:29 +0400

    [TEST] Added tests to check if list satisfy to the condition.

--- src/test/java/xogame/game_over/GameOverTest.java
+++ src/test/java/xogame/game_over/GameOverTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +15,8 @@ import org.junit.jupiter.api.Test;
 import xogame.models.Board;
 
 public class GameOverTest {
+    public final Predicate<Integer> condition = item -> item == 1 || item == 2;
+    public final List<Integer> emptyList = Collections.<Integer>emptyList();
 
     @Test
     public void get_columns_of_the_board() {
@@ -35,5 +40,65 @@ public class GameOverTest {
         assertFalse(GameOver.isBitwiseOrAssignmentOperator(false, false));
         assertTrue(GameOver.isBitwiseOrAssignmentOperator(false, true));
         assertTrue(GameOver.isBitwiseOrAssignmentOperator(true, true));
+    }
+
+    @Test
+    public void list_contains_one_empty_list_and_it_is_not_satisfy_to_the_condition() {
+        assertFalse(GameOver.isListSatisfyToTheCondition(Arrays.asList(emptyList), condition));
+    }  
+
+    @Test
+    public void list_contains_two_empty_lists_and_it_is_not_satisfy_to_the_condition() {
+        assertFalse(GameOver.isListSatisfyToTheCondition(Arrays.asList(emptyList, emptyList), condition));
+    }
+
+    @Test
+    public void list_contains_one_not_empty_list_and_it_is_satisfy_to_the_condition() {
+        assertTrue(GameOver.isListSatisfyToTheCondition(Arrays.asList(Arrays.asList(1, 1, 1)), condition));
+    }
+
+    @Test
+    public void list_contains_two_not_empty_lists_and_it_is_satisfy_to_the_condition() {
+        assertTrue(GameOver.isListSatisfyToTheCondition(Arrays.asList(Arrays.asList(1, 1, 1), Arrays.asList(2, 2, 2)), condition));
+    }
+
+    @Test
+    public void list_contains_one_empty_and_one_not_empty_lists_and_it_is_satisfy_to_the_condition() {
+        assertTrue(GameOver.isListSatisfyToTheCondition(Arrays.asList(emptyList, Arrays.asList(2, 2, 2)), condition));
+    }
+
+    @Test
+    public void list_contains_one_empty_and_two_not_empty_lists_and_it_is_satisfy_to_the_condition() {
+        assertTrue(GameOver.isListSatisfyToTheCondition(Arrays.asList(emptyList, Arrays.asList(2, 2, 2), Arrays.asList(2, 2, 2)), condition));
+    }
+
+    @Test
+    public void list_contains_one_not_empty_and_one_empty_lists_and_it_is_satisfy_to_the_condition() {
+        assertTrue(GameOver.isListSatisfyToTheCondition(Arrays.asList(Arrays.asList(2, 2, 2), emptyList), condition));
+    }
+
+    @Test
+    public void list_contains_one_not_empty_and_two_empty_lists_and_it_is_satisfy_to_the_condition() {
+        assertTrue(GameOver.isListSatisfyToTheCondition(Arrays.asList(Arrays.asList(2, 2, 2), emptyList, emptyList), condition));
+    }
+
+    @Test
+    public void list_contains_two_empty_and_one_not_empty_lists_and_it_is_satisfy_to_the_condition() {
+        assertTrue(GameOver.isListSatisfyToTheCondition(Arrays.asList(emptyList, emptyList, Arrays.asList(2, 2, 2)), condition));
+    }
+
+    @Test
+    public void list_contains_two_not_empty_and_one_empty_lists_and_it_is_satisfy_to_the_condition() {
+        assertTrue(GameOver.isListSatisfyToTheCondition(Arrays.asList(Arrays.asList(2, 2, 2), Arrays.asList(2, 2, 2), emptyList), condition));
+    }
+
+    @Test
+    public void list_contains_one_not_empty_and_one_empty_and_one_not_empty_lists_and_it_is_satisfy_to_the_condition() {
+        assertTrue(GameOver.isListSatisfyToTheCondition(Arrays.asList(Arrays.asList(2, 2, 2), emptyList, Arrays.asList(2, 2, 2)), condition));
+    }
+
+    @Test
+    public void list_contains_one_empty_and_one_not_empty_and_one_empty_lists_and_it_is_satisfy_to_the_condition() {
+        assertTrue(GameOver.isListSatisfyToTheCondition(Arrays.asList(emptyList, Arrays.asList(2, 2, 2), emptyList), condition));
     }
 }
