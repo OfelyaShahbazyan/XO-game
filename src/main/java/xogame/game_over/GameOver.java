4431fcc0b5c4fd4fed18eb86226d009db9802514
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-10-04 13:00:20 +0400

    [REFACTOR] Changed name of method.

--- src/main/java/xogame/game_over/GameOver.java
+++ src/main/java/xogame/game_over/GameOver.java
@@ -9,10 +9,10 @@ public class GameOver {
     public static final Predicate<Integer> streamsPredicate = item -> item == 1 || item == 2;
 
     public static Boolean isAnyRowComplete(Board board) {
-        return isBitwiseOrAssignmentOperator(false, isListSatisfyToTheCondition(board.getRows(), streamsPredicate));
+        return isBitwiseOrAssignmentOperator(false, doesListSatisfyToTheCondition(board.getRows(), streamsPredicate));
     }
 
-    public static Boolean isListSatisfyToTheCondition(List<List<Integer>> list, Predicate<Integer> condition) {
+    public static Boolean doesListSatisfyToTheCondition(List<List<Integer>> list, Predicate<Integer> condition) {
         Boolean result = false;
 
         for (int i = 0; i < list.size(); i++) {
