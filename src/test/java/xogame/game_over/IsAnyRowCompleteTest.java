28eec894a1262fff741f597a4c76a513514d7eda
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-10-06 18:18:20 +0400

    [REFACTOR]

--- src/test/java/xogame/game_over/IsAnyRowCompleteTest.java
+++ src/test/java/xogame/game_over/IsAnyRowCompleteTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import xogame.models.Board;
 
-public class GameOverTest {
+public class IsAnyRowCompleteTest {
     @Test
     public void the_first_row_is_complete_if_it_contains_all_ones() {
         Board board = new Board(new int[] { 1, 1, 1, 2, 2, 0, 0, 0, 0 });
