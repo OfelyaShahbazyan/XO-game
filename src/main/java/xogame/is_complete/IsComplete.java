754549d8c9749b5be9eaf479969bf790d324fa4d
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-10-07 19:12:34 +0400

    [REFACTOR] Improved packaging related to the rows.

--- src/main/java/xogame/is_complete/IsComplete.java
+++ src/main/java/xogame/is_complete/IsComplete.java
@@ -1,23 +1,23 @@
-package xogame.game_over;
+package xogame.is_complete;
 
 import java.util.List;
 import java.util.function.Predicate;
 
 import xogame.models.Board;
 
-public class GameOver {
-    public static final Predicate<List<Integer>> listPredicate = list -> list.stream().allMatch(e -> e == 1) || list.stream().allMatch(e -> e == 2);
+public class IsComplete {
+    public static final Predicate<List<Integer>> LIST_PREDICATE = list -> list.stream().allMatch(e -> e == 1) || list.stream().allMatch(e -> e == 2);
 
     public static Boolean isAnyRowComplete(Board board) {
-        return doesAnyInnerListMatchTheCondition(board.getRows(), listPredicate);
-    }
-
+        return IsComplete.doesAnyInnerListMatchTheCondition(board.getRows(), LIST_PREDICATE);
+    }    
+    
     public static Boolean isAnyColumnComplete(Board board) {
-        return doesAnyInnerListMatchTheCondition(board.getColumns(), listPredicate);
+        return doesAnyInnerListMatchTheCondition(board.getColumns(), LIST_PREDICATE);
     }
 
     public static Boolean isAnyDiagonalComplete(Board board) {
-        return doesAnyInnerListMatchTheCondition(board.getDiagonals(), listPredicate);
+        return doesAnyInnerListMatchTheCondition(board.getDiagonals(), LIST_PREDICATE);
     }
 
     public static Boolean doesAnyInnerListMatchTheCondition(List<List<Integer>> listOfLists, Predicate<List<Integer>> condition) {
