30cee056b0e18d97be66f6b68b6b444df1c0b0e2
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-10-07 22:41:24 +0400

    [FEAT] Implemented logic to check if board contains at least one empty cell.

---
+++ src/main/java/xogame/board_utils/BoardUtils.java
@@ -0,0 +1,9 @@
+package xogame.board_utils;
+
+import xogame.models.Board;
+
+public class BoardUtils {
+    public static Boolean doesBoardContainAtLeastOneEmptyCell(Board board){
+        return null;
+    }    
+}
