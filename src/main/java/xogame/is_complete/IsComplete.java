3f86ac0555394011b28e19b823ca4270131060fb
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-10-08 15:19:15 +0400

    [REFACTOR]

--- src/main/java/xogame/is_complete/IsComplete.java
+++ src/main/java/xogame/is_complete/IsComplete.java
@@ -7,17 +7,17 @@ import xogame.list_utils.ListUtils;
 import xogame.models.Board;
 
 public class IsComplete {
-    public static final Predicate<List<Integer>> LIST_PREDICATE = list -> list.stream().allMatch(e -> e == 1) || list.stream().allMatch(e -> e == 2);
+    public static final Predicate<List<Integer>> listPredicate = list -> list.stream().allMatch(e -> e == 1) || list.stream().allMatch(e -> e == 2);
 
     public static Boolean isAnyRowComplete(Board board) {
-        return ListUtils.doesAnyInnerListMatchTheCondition(board.getRows(), LIST_PREDICATE);
+        return ListUtils.doesAnyInnerListMatchTheCondition(board.getRows(), listPredicate);
     }
 
     public static Boolean isAnyColumnComplete(Board board) {
-        return ListUtils.doesAnyInnerListMatchTheCondition(board.getColumns(), LIST_PREDICATE);
+        return ListUtils.doesAnyInnerListMatchTheCondition(board.getColumns(), listPredicate);
     }
 
     public static Boolean isAnyDiagonalComplete(Board board) {
-        return ListUtils.doesAnyInnerListMatchTheCondition(board.getDiagonals(), LIST_PREDICATE);
+        return ListUtils.doesAnyInnerListMatchTheCondition(board.getDiagonals(), listPredicate);
     }
 }
