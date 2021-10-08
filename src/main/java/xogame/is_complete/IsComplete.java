369ae1fb59241e985f2372713ad0db93ebfdc219
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-10-08 21:30:05 +0400

    [FEAT] Implemented method to check if complete row/column/diagonal/board.

--- src/main/java/xogame/is_complete/IsComplete.java
+++ src/main/java/xogame/is_complete/IsComplete.java
@@ -3,12 +3,18 @@ package xogame.is_complete;
 import java.util.List;
 import java.util.function.Predicate;
 
+import xogame.board_utils.BoardUtils;
 import xogame.list_utils.ListUtils;
 import xogame.models.Board;
 
 public class IsComplete {
     public static final Predicate<List<Integer>> listPredicate = list -> list.stream().allMatch(e -> e == 1) || list.stream().allMatch(e -> e == 2);
 
+    public static Boolean isComplete(Board board){
+        return isAnyRowComplete(board) || isAnyColumnComplete(board) ||
+                isAnyDiagonalComplete(board) || !BoardUtils.doesBoardContainAtLeastOneEmptyCell(board);
+    }    
+    
     public static Boolean isAnyRowComplete(Board board) {
         return ListUtils.doesAnyInnerListMatchTheCondition(board.getRows(), listPredicate);
     }
