769d885747bd8f71d1e378986ac89170064d399a
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-10-10 04:17:19 +0400

    [improvement](Extra) Rewrited the body of the method getRows() using the Java stream reduce().

--- src/main/java/xogame/list_utils/ListUtils.java
+++ src/main/java/xogame/list_utils/ListUtils.java
@@ -5,6 +5,11 @@ import java.util.function.Predicate;
 
 public class ListUtils {
     public static Boolean doesAnyInnerListMatchTheCondition(List<List<Integer>> listOfLists, Predicate<List<Integer>> condition) {
-        return listOfLists.stream().filter(e -> !e.isEmpty()).anyMatch(condition);
+        Boolean identity = false;
+
+        return listOfLists.stream().reduce(identity,
+                (partialResult, lists) -> listOfLists.stream().filter(e -> !e.isEmpty()).anyMatch(condition),
+                (a, b) -> a |= b);
+        // return listOfLists.stream().filter(e -> !e.isEmpty()).anyMatch(condition);
     }
 }
