a339aca45946cd9036b2ece695534e165e75ac9c
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-10-13 01:01:34 +0400

    [FEAT] Implemented logic to fill each row by the given symbol and  generate board for each case.

--- src/main/java/xogame/board_utils/Generator.java
+++ src/main/java/xogame/board_utils/Generator.java
@@ -1,11 +1,24 @@
 package xogame.board_utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import xogame.models.Board;
 
 public class Generator {
     public static List<Board> fill_in_row_of_a_board_with_symbol(int symbol, int board_size) {
-        return null;
+        List<Board> listOfBoard = new ArrayList<Board>();
+        Board board = new Board();
+
+        for (int i = 0; i < board_size; i++) {
+            int[] generatedBoard = board.getBoard();
+            generatedBoard[i * 3] = symbol;
+            generatedBoard[i * 3 + 1] = symbol;
+            generatedBoard[i * 3 + 2] = symbol;
+
+            listOfBoard.add(new Board(generatedBoard));
+        }
+
+        return listOfBoard;
     }
 }
