88921e0c53919c7037752b696fbdc4ce2ae588e5
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-10-14 01:59:45 +0400

    [FEAT] Implemented method to fill nth row of the board with given symbol.

--- src/main/java/xogame/board_utils/Generator.java
+++ src/main/java/xogame/board_utils/Generator.java
@@ -1,10 +1,20 @@
 package xogame.board_utils;
 
+import java.util.Arrays;
+
 import xogame.models.Board;
 import xogame.utils.Result;
 
 public class Generator {
     public static Result<Board> fill_nth_row_of_the_board_with_symbol(int board_size, int rowNumber, int symbol) {
-       return null;
+        int[] array = new int[board_size * board_size];
+
+        if (rowNumber < board_size) {
+            Arrays.fill(array, board_size * rowNumber, (rowNumber + 1) * board_size, symbol);
+
+            return new Result<Board>(new Board(array));
+        } else {
+            return new Result<Board>(new Board(array), "There is no row like this.");
+        }
     }
 }
