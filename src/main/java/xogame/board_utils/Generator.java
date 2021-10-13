f91cbcfd501c67de4b9791672f5144c452485103
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-10-14 01:57:45 +0400

    [TEST] Added tests to check if filling nth row of the board with given symbol fail or succeed to match.

--- src/main/java/xogame/board_utils/Generator.java
+++ src/main/java/xogame/board_utils/Generator.java
@@ -1,24 +1,10 @@
 package xogame.board_utils;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import xogame.models.Board;
+import xogame.utils.Result;
 
 public class Generator {
-    public static List<Board> fill_in_row_of_a_board_with_symbol(int symbol, int board_size) {
-        List<Board> listOfBoard = new ArrayList<Board>();
-        Board board = new Board();
-
-        for (int i = 0; i < board_size; i++) {
-            int[] generatedBoard = board.getBoard();
-            generatedBoard[i * 3] = symbol;
-            generatedBoard[i * 3 + 1] = symbol;
-            generatedBoard[i * 3 + 2] = symbol;
-
-            listOfBoard.add(new Board(generatedBoard));
-        }
-
-        return listOfBoard;
+    public static Result<Board> fill_nth_row_of_the_board_with_symbol(int board_size, int rowNumber, int symbol) {
+       return null;
     }
 }
--- src/test/java/xogame/board_utils/GeneratorTest.java
+++ src/test/java/xogame/board_utils/GeneratorTest.java
@@ -2,53 +2,38 @@ package xogame.board_utils;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
+import xogame.models.Board;
+import xogame.utils.Result;
+
 public class GeneratorTest {
     @Test
-    public void count_of_boards_containing_rows_all_ones_should_be_three() {
-        assertEquals(3, Generator.fill_in_row_of_a_board_with_symbol(1, 3).size());
+    public void filling_first_row_of_the_board_with_ones_should_succeed_to_match() {
+        assertArrayEquals(new int[] { 1, 1, 0, 0 },
+                Generator.fill_nth_row_of_the_board_with_symbol(2, 0, 1).value.getBoard());
     }
 
     @Test
-    public void count_of_boards_containing_rows_all_twos_should_be_three() {
-        assertEquals(3, Generator.fill_in_row_of_a_board_with_symbol(2, 3).size());
+    public void filling_second_row_of_the_board_with_twos_should_succeed_to_match() {
+        assertArrayEquals(new int[] { 0, 0, 2, 2 },
+                Generator.fill_nth_row_of_the_board_with_symbol(2, 1, 2).value.getBoard());
     }
 
     @Test
-    public void board_containing_first_row_all_ones_should_succeed_to_match() {
-        assertArrayEquals(new int[] { 1, 1, 1, 0, 0, 0, 0, 0, 0 },
-                Generator.fill_in_row_of_a_board_with_symbol(1, 3).get(0).getBoard());
-    }
-
-    @Test
-    public void board_containing_second_row_all_ones_should_succeed_to_match() {
-        assertArrayEquals(new int[] { 0, 0, 0, 1, 1, 1, 0, 0, 0 },
-                Generator.fill_in_row_of_a_board_with_symbol(1, 3).get(1).getBoard());
-    }
-
-    @Test
-    public void board_containing_third_row_all_ones_should_succeed_to_match() {
+    public void filling_third_row_of_the_board_with_ones_should_succeed_to_match() {
         assertArrayEquals(new int[] { 0, 0, 0, 0, 0, 0, 1, 1, 1 },
-                Generator.fill_in_row_of_a_board_with_symbol(1, 3).get(2).getBoard());
+                Generator.fill_nth_row_of_the_board_with_symbol(3, 2, 1).value.getBoard());
     }
 
     @Test
-    public void board_containing_first_row_all_twos_should_succeed_to_match() {
-        assertArrayEquals(new int[] { 2, 2, 2, 0, 0, 0, 0, 0, 0 },
-                Generator.fill_in_row_of_a_board_with_symbol(2, 3).get(0).getBoard());
-    }
+    public void filling_sixth_row_of_the_board_with_ones_should_fail_to_match() {
+        Result<Board> res = Generator.fill_nth_row_of_the_board_with_symbol(3, 5, 1);
 
-    @Test
-    public void board_containing_second_row_all_twos_should_succeed_to_match() {
-        assertArrayEquals(new int[] { 0, 0, 0, 2, 2, 2, 0, 0, 0 },
-                Generator.fill_in_row_of_a_board_with_symbol(2, 3).get(1).getBoard());
-    }
-
-    @Test
-    public void board_containing_third_row_all_twos_should_succeed_to_match() {
-        assertArrayEquals(new int[] { 0, 0, 0, 0, 0, 0, 2, 2, 2 },
-                Generator.fill_in_row_of_a_board_with_symbol(2, 3).get(2).getBoard());
+        assertArrayEquals(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, res.value.getBoard());
+        assertEquals("There is no row like this.", res.errorMessage);
+        assertFalse(res.isSuccess);
     }
 }
