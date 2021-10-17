f9a80b68fad9c1f5d45698963297cb04a2963b91
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-10-17 21:05:25 +0400

    [IMPROVEMENT] Changed implementation of the move functionality.

--- src/main/java/xogame/models/Move.java
+++ src/main/java/xogame/models/Move.java
@@ -5,14 +5,14 @@ import xogame.utils.Result;
 public class Move {
   public static Result<Board> move(Coordinate coordinate, int symbol, Board board) {
     int idx = coordinate.x + 3 * coordinate.y;
+    Result<Board> result = new Result<Board>(board, "Cell has been taken.");
 
     if (board.getBoard()[idx] == 0) {
       int[] newBoard = board.getBoard();
       newBoard[coordinate.x + 3 * coordinate.y] = symbol;
-      
-      return new Result<Board>(new Board(newBoard));
-    } else {
-      return new Result<Board>(board, "Cell has been taken.");
+      result = new Result<Board>(new Board(newBoard));
     }
+
+    return result;
   }
 }
