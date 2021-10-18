f289fad6c639fbe55f4db20b2341d49779d8bd2e
OfelyaShahbazyan <shahbazyanofelya.9@gmail.com>
2021-10-18 15:48:43 +0400

    [IMPROVEMENT] Rewrited few lines of the code more easy/short way.

--- src/main/java/xogame/models/Board.java
+++ src/main/java/xogame/models/Board.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Board {
     private final int[] board;
